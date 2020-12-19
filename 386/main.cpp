284f6703ad019085a68c1cdfdd0cc440041d60b4
Bulat Gabdrakhmanov <bulat.gab2000@gmail.com>
2020-12-19 18:57:53 +0300

    Переделал 386

--- 386/main.cpp
+++ 386/main.cpp
@@ -10,9 +10,20 @@ int main() {
         int n;
         input >> n;
 
+        int x = 0;
+        int y = -5000;
+
         output << "YES" << endl;
-        for (int i = 0; i < n; i++) {
-            output << rand() % 20001 - 10000 << ' ' << rand() % 20001 - 10000 << endl;
+        if (n % 2 == 1) {
+            output << x << ' ' << y << endl;
+            n--;
+        }
+
+        for (int i = 1; i <= n / 2; i++) {
+            x = x + 1;
+            y = y + i;
+            output << x << ' ' << y << endl;
+            output << -x << ' ' << y << endl;
         }
     }
 
